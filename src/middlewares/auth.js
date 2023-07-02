07af1d177cb3412e0090e78c0fdea66438b4d46d
Diego BustamantediegoBustamante <diegi.b58@gmail.com>
2023-07-01 23:48:47 -0300

    entrega login

---
+++ src/middlewares/auth.js
@@ -0,0 +1,26 @@
+export function checkUser(req, res, next) {
+    if (req.session.email) {
+        return next();
+    }
+    return res.status(401).render('error-page', {
+        msg: 'please log in'
+    });
+}
+
+export function checkAdmin(req, res, next) {
+    if (req.session.email && req.session.admin == true) {
+        return next();
+    }
+    return res.status(401).render('error-page', {
+        msg: 'please log in AS ADMIN!'
+    });
+}
+
+export function checkUserLoggedIn(req, res, next) {
+    if (!req.session.email) {
+        return next();
+    }
+    return res.status(401).render('error-page', {
+        msg: 'You are already logged in'
+    });
+}
---
+++ src/routes/views.router.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { checkAdmin, checkUser, checkUserLoggedIn } from '../middlewares/auth.js';
+export const routerViews = express.Router();
+
+routerViews.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.render('error-page', {
+                msg: 'could not close the session'
+            });
+        }
+        return res.redirect('/login');
+    });
+});
+
+routerViews.get('/login', checkUserLoggedIn, (req, res) => {
+    res.render('login-form');
+});
+
+routerViews.get('/register', checkUserLoggedIn, (req, res) => {
+    res.render('register-form');
+});
+
+routerViews.get('/profile', checkUser, (req, res) => {
+    res.render('profile');
+});
+
+routerViews.get('/admin', checkAdmin, (req, res) => {
+    res.send("Welcome to the secret place, if you got here it's because you're an admin!");
+});
